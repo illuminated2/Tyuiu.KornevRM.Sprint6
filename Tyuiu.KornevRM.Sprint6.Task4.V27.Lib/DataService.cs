13dfcaf3def2bbc6ed70c73c8f6a695560ca3972
illuminated <iLLuminated_89@mail.ru>
2024-12-09 01:19:55 +0500

    Task4

---
+++ Tyuiu.KornevRM.Sprint6.Task4.V27.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.KornevRM.Sprint6.Task4.V27.Lib
+{
+    public class DataService : ISprint6Task4V27
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] result = new double[(stopValue - startValue) + 1];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == -1)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y = Math.Round((Math.Cos(x) / (x + 1) - Math.Cos(x) * 1.3 + 3 * x), 2);
+                }
+
+                result[count++] = y;
+            }
+            return result;
+        }
+    }
+}
